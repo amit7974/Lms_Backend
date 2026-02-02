b674b45af634a877ccf5dba757365fa5624136bf
Er. Amit Kumar <121847237+amit7974@users.noreply.github.com>
2026-02-02 22:36:54 +0530

    Add files via upload

---
+++ ZoneRepository.java
@@ -0,0 +1,7 @@
+package com.coder_amit.repositories;
+
+import com.coder_amit.model.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}
